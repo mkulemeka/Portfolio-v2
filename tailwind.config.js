84f1cd2b42ae4e0c887ce50bb5583c41dbd5b9ad
mkulemeka <kahlekulemeka@gmail.com>
2023-04-19 21:16:54 -0400

    done setting styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      green: "#49c5b6",
+      black: "#26282A",
+      fadedBlack: "#1D1D1D",
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+    },
   },
   plugins: [],
 };
