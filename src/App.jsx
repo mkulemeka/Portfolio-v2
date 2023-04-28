a750c45af2cdfcf3aac2b401014a02fa28f4489f
mkulemeka <kahlekulemeka@gmail.com>
2023-04-27 22:08:37 -0400

    framer animation setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { About, Contact, Home, Projects } from "./pages";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 
+import { AnimatePresence } from "framer-motion";
 import { Navbar } from "./components";
 import React from "react";
 
 const App = () => {
+  const location = useLocation();
   return (
     <section className="bg-fadedBlack min-h-[100vh] text-white text-regular">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.key}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </AnimatePresence>
     </section>
   );
 };
