8bd43943e31644a669425491330ec50a3b986374
mkulemeka <kahlekulemeka@gmail.com>
2023-04-27 21:13:44 -0400

    navigation styling complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,15 +24,25 @@ const Navbar = () => {
   ];
   return (
     <>
-      <nav>
-        <div>
+      <nav className="w-[8rem] md:fixed z-10 h-[100%] flex flex-col items-center justify-center gap-10">
+        <div className="w-[50%] bg-fadedMoreBlack flex flex-col items-center gap-3 p-2 rounded-[3rem]">
           {pages.map((page) => (
-            <Link to={page[0]}>{page[1]}</Link>
+            <Link
+              to={page[0]}
+              className="p-4 rounded-full hover:bg-fadedBlack transition ease-in-out duration-300"
+            >
+              {page[1]}
+            </Link>
           ))}
         </div>
-        <div>
+        <div className="w-[50%] bg-fadedMoreBlack flex flex-col items-center gap-3 p-2 rounded-[3rem]">
           {socialLinks.map((socialLink) => (
-            <a href={socialLink.link}>{socialLink.tag}</a>
+            <a
+              href={socialLink.link}
+              className="p-4 rounded-full hover:bg-green transition ease-in-out duration-300"
+            >
+              {socialLink.tag}
+            </a>
           ))}
         </div>
       </nav>
